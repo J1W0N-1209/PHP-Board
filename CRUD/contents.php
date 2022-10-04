ebbd1284dd55c6d0950a588db494dd9b9d6952c5
jiwonKang <nabook754@naver.com>
2022-10-05 00:26:41 +0900

    contents

--- CRUD/contents.php
+++ CRUD/contents.php
@@ -1,3 +1,18 @@
 <?php
     include "lib.php";
+
+    if(isset($_GET['idx'])) {
+        $idx = $_GET['idx'];
+        $sql = "select * from post where idx=$idx";
+
+        $result = mysqli_query($connect,$sql);
+        $data = mysqli_fetch_array($result);
+
+        print_r($data);
+    }
+    else {
+        die("idx 값을 넣어주세요 :)");
+    }
+
+
 ?>
